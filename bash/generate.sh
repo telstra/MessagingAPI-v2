d843e833670d712e86ebb21c50f1c8cbd232322a
developersteve <steven@developersteve.com>
2018-02-06 12:15:12 +1100

    Spec updated to 2.2.4

--- bash/generate.sh
+++ bash/generate.sh
@@ -12,9 +12,29 @@ do
 
     	mkdir -p ../../test/$fname
 
-		eval swagger-codegen generate -i ../docs/swagger/messaging-api-swagger.yaml -l $fname -c $file -o ../../test/$fname
+		eval swagger-codegen generate -i ../docs/swagger/messaging-api-swagger.yaml -l $fname -c $file -o $folder/$fname 
+    fi
+done
 
-		rm ../../test/$fname/git_push.sh
+for file in ../spec_config/*.json
+do	
+    if [[ -f $file ]]; then
+
+    	s=${file##*/}
+    	fname=${s%.*}
+
+		if [ $fname = "php" ]; then
+			eval mv -f $folder/$fname/SwaggerClient-php/* $folder/$fname/
+			eval rm -rf $folder/$fname/SwaggerClient-php
+			# eval ./vendor/bin/phpunit --bootstrap vendor/autoload.php test/
+		elif [ $fname = "python" ]; then
+			# pip install -r test-requirements
+			# nosetests ./test/
+			echo "python"
+		elif [ $fname = "java" ]; then 
+			# mvn integration-test
+			echo "Java"
+		fi
 
 		fileloc="../../test/"
 		fileloc+=$fname
@@ -25,9 +45,22 @@ do
 			sed -i '' "/$Swagger Codegen/d" $folder/$fname/README.md 
 			sed -i '' "/$Build package/d" $folder/$fname/README.md
 			sed -i '' "/$swagger-codegen/d" $folder/$fname/README.md
+			sed -i '' "/$swagger/d" $folder/$fname/README.md
+			sed -i '' "/$Swagger/d" $folder/$fname/README.md
+			sed -i '' "/$SwaggerClient-php/d" $folder/$fname/README.md
 			sed -i '' "/$Author/d" $folder/$fname/README.md
 		fi
 
+		rm ../../test/$fname/git_push.sh
+
+		# Runtests
+		# if[$file="python"]
+		# then
+			# eval pip install -r ../../test/$fname/test-requirements.txt
+		# elif[$file=""]
+		# then
+
+		# fi
+
     fi
 done
-
